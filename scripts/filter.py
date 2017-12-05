5fff6e850641d55a7901ce51d6c7cb9eabe6f5b5
Administrator <vgilles@et.esiea.fr>
2017-12-05 16:08:48 +0100

    [ADD] it's not working

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 
 def filter(data, filter):
+	count = 0
 	filtered = []
-	for item in data:
-		#print(item['date'])
-		if datetime.strptime(item['date'], '%m-%d-%Y %H:%M:%S').hour == 19:
-			filtered.append(item)
-	print(filtered)
+	for item in range(8760):
+		if datetime.strptime(data[item]['date'], '%m-%d-%Y %H:%M:%S').hour == filter and datetime.strptime(data[item]['date'], '%m-%d-%Y %H:%M:%S').weekday() != 5 and datetime.strptime(data[item]['date'], '%m-%d-%Y %H:%M:%S') != 6:
+			filtered.append(data[item])
+			count += 1
+#	for item in data:
+#		if datetime.strptime(item['date'], '%m-%d-%Y %H:%M:%S').hour == filter and datetime.strptime(item['date'], '%m-%d-%Y %H:%M:%S').weekday() != 5 and datetime.strptime(item['date'], '%m-%d-%Y %H:%M:%S').weekday() != 6:
+#			filtered.append(item)
+#			count += 1
+	print(count)
 	return filtered
--- scripts/run.py
+++ scripts/run.py
@@ -2,9 +2,11 @@ import parseCosts
 import filter
 
 def run():
+	print('****************')
 	costsSum = 0
 	filtered = filter.filter(parseCosts.parse(), 19)
 	for item in filtered:
+		print(item)
 		costsSum += float(item['spot'])
-	print(costsSum)
+	print(costsSum*6*0.001)
 run()
