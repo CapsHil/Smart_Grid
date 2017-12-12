491f50d02e58397dc9ca63488eb7bacc06066611
Administrator <vgilles@et.esiea.fr>
2017-12-12 15:08:28 +0100

    [CLEAN] run3.py

--- scripts/run3.py
+++ scripts/run3.py
@@ -3,7 +3,7 @@ import filter as f
 from datetime import datetime
 
 sum = 0
-data = f.filterByLowestByDate(f.filterfrom19to05(f.filterWeekends(p.parse())))
+data = f.filterByLowestByDate(f.filterfrom19to05(p.parse()))
 for value in data:
     sum += float(value)*6/1000
 print(sum)
